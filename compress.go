21f4e89cbb5ece162a5676a94b4d9183c0f5e901
kaf <897243839@qq.com>
2023-05-25 17:00:42 +0800

    Initial commit

---
+++ compress.go
@@ -0,0 +1,33 @@
+package cudacomp
+
+// #cgo LDFLAGS: -llz4decom
+//#include "lz4_cpu.h"
+/*
+#include <stdio.h>
+#include <stdlib.h>
+*/
+import "C"
+import (
+	"unsafe"
+)
+
+// lz4解压缩
+func Cuda_Lz4_compress(val []byte) (value []byte) {
+
+	var size C.size_t
+	cByteStream := C.go_data_comp((*C.char)(unsafe.Pointer(&val[0])), C.int(len(val)), &size)
+	// Convert the C array to a Go slice
+	buf := (*[1 << 30]byte)(unsafe.Pointer(cByteStream))[:size:size]
+
+	return buf
+}
+
+func Cuda_Lz4_decompress(data []byte) (value []byte) {
+	//---------------------------解压
+	var size C.size_t
+	cByteStream := C.go_data_re((*C.char)(unsafe.Pointer(&data[0])), C.int(len(data)), &size)
+	// Convert the C array to a Go slice
+	out := (*[1 << 30]byte)(unsafe.Pointer(cByteStream))[:size:size]
+
+	return out
+}
---
+++ lz4_cpu.h
@@ -0,0 +1,12 @@
+
+// Benchmark performance from the binary data file fname
+
+// 定义 CChar 类
+#define CChar unsigned char
+int  call_run_example();
+int test();
+void print_string(char *s);
+int  run_example(char *s);
+int go_data(char* data, int len);
+char* go_data_re(char* data, int len,size_t* size);
+char* go_data_comp(char* data, int len,size_t* size);
